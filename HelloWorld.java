a474aae6c83caa50eefed2eb30eeb234755a45ce
mangesh4558 <mangeshabnave@yahoo.com>
2018-09-16 01:36:31 +0530

    fix for TES-2
    fix for TES-2

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,6 @@ package com.main;
  class HelloWorld {
    public static void main(String[] args) {
       // Prints "Hello, World" in the terminal window.
-      System.out.println("Hello, World 1");
+      System.out.println("Hello, World 2");
    }
 }
